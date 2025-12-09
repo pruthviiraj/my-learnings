a80c0fc9e16f380c916cc1c3609e5bdc7584e99a
Pruthvi Raj Pudi <42543442+pruthviiraj@users.noreply.github.com>
2025-12-09 04:22:09 -0600

    Add SQL query to select names by occupation

---
+++ HackerRank/Day_2/occupations.sql
@@ -0,0 +1,14 @@
+SELECT
+    MAX(CASE WHEN occupation = 'Doctor' THEN name END) AS Doctor,
+    MAX(CASE WHEN occupation = 'Professor' THEN name END) AS Professor,
+    MAX(CASE WHEN occupation = 'Singer' THEN name END) AS Singer,
+    MAX(CASE WHEN occupation = 'Actor' THEN name END) AS Actor
+FROM(
+    SELECT
+        name,
+        occupation,
+        ROW_NUMBER() OVER(PARTITION BY occupation ORDER BY name) AS row_num
+FROM occupations
+) t
+GROUP BY row_num
+ORDER BY row_num;
