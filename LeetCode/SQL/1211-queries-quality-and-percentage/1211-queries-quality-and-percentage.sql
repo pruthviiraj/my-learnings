882fe5bd027fcb3f0d6d3592114be5f39510715f
Pruthvi Raj Pudi <42543442+pruthviiraj@users.noreply.github.com>
2026-07-14 03:06:25 -0500

    Add LeetCode solution: Queries Quality and Percentage

--- LeetCode/SQL/1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ LeetCode/SQL/1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 SELECT
     query_name,
     ROUND(AVG(rating/position),2) AS quality,
-    ROUND((COUNT(rating)<=3)*100.0/COUNT(rating),2) AS poor_query_percentage
+    ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0,2) AS poor_query_percentage
 FROM queries
 GROUP BY query_name;
